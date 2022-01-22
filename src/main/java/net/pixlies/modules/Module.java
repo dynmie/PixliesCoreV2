4f17192320bad54047d03cd4028b40ffc8334b0c
dynmie <xbottam@gmail.com>
2022-01-21 19:13:31 -0800

    Fix getters and setter, remove some stuff because there was already Main.getInstance();

--- src/main/java/net/pixlies/modules/Module.java
+++ src/main/java/net/pixlies/modules/Module.java
@@ -9,11 +9,10 @@ import java.util.logging.Logger;
 
 public abstract class Module {
 
-    @Getter(AccessLevel.PROTECTED) private static final Main pixlies = Main.getInstance();
     @Getter private final ModuleDescription description = Main.getInstance().getModuleManager().getDescription(this);
-    @Getter(AccessLevel.PROTECTED) private final File moduleFolder = new File(
+    @Getter private final File moduleFolder = new File(
             Main.getInstance().getDataFolder().getAbsolutePath() + File.separator + "modules" + File.separator + description.getName());
-    @Getter(AccessLevel.PROTECTED) private final Logger logger = Logger.getLogger(description.getName());
+    @Getter private final Logger logger = Logger.getLogger(description.getName());
 
     public abstract void onLoad();
 
