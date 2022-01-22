1162495f07c0e861167cb66c53d0047de0ea206f
dynmie <xbottam@gmail.com>
2022-01-21 18:56:23 -0800

    change method name to getPixlies

--- src/main/java/net/pixlies/modules/Module.java
+++ src/main/java/net/pixlies/modules/Module.java
@@ -8,7 +8,7 @@ import java.io.File;
 
 public abstract class Module {
 
-    @Getter(AccessLevel.PROTECTED) private static final Main instance = Main.getInstance();
+    @Getter(AccessLevel.PROTECTED) private static final Main pixlies = Main.getInstance();
     @Getter private final ModuleDescription description = Main.getInstance().getModuleManager().getDescription(this);
     @Getter(AccessLevel.PROTECTED) private final File moduleFolder = new File(
             Main.getInstance().getDataFolder().getAbsolutePath() + File.separator + "modules" + File.separator + description.getName());
