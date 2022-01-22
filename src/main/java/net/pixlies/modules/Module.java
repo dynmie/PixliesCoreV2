8ef49e3cdb37a62e7d03a5d2f723bada20d07745
dynmie <xbottam@gmail.com>
2022-01-21 18:14:28 -0800

    add getModuleFolder

--- src/main/java/net/pixlies/modules/Module.java
+++ src/main/java/net/pixlies/modules/Module.java
@@ -4,10 +4,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import net.pixlies.Main;
 
+import java.io.File;
+
 public abstract class Module {
 
     @Getter(AccessLevel.PROTECTED) private static final Main instance = Main.getInstance();
     @Getter private final ModuleDescription description = Main.getInstance().getModuleManager().getDescription(this);
+    @Getter(AccessLevel.PROTECTED) private final File moduleFolder = new File(
+            Main.getInstance().getDataFolder().getAbsolutePath() + File.separator + "modules" + File.separator + description.getName());
 
     public abstract void onLoad();
 
