7ba024b6c2450520e2fdc9c649b732dbbd6f69e8
dynmie <xbottam@gmail.com>
2022-01-23 14:15:23 -0800

    Changed classloader getResource thing

--- Nations/src/main/java/net/pixlies/nations/config/Config.java
+++ Nations/src/main/java/net/pixlies/nations/config/Config.java
@@ -1,7 +1,6 @@
 package net.pixlies.nations.config;
 
 import net.pixlies.nations.Nations;
-import net.pixlies.nations.utils.ResourceReader;
 import org.apache.commons.io.FileUtils;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -24,7 +23,7 @@ public class Config extends YamlConfiguration {
         this.file = file;
         this.localDefaultsName = localDefaultsName;
         try {
-            Reader stream = new InputStreamReader(Objects.requireNonNull(ResourceReader.getResource(localDefaultsName)), StandardCharsets.UTF_8);
+            Reader stream = new InputStreamReader(Objects.requireNonNull(instance.getResource(localDefaultsName)), StandardCharsets.UTF_8);
             YamlConfiguration config = new YamlConfiguration();
             config.load(stream);
             this.setDefaults(config);
@@ -73,7 +72,7 @@ public class Config extends YamlConfiguration {
         }
 
         try {
-            FileUtils.copyInputStreamToFile(Objects.requireNonNull(ResourceReader.getResource(localDefaultsName)), file);
+            FileUtils.copyInputStreamToFile(Objects.requireNonNull(instance.getResource(localDefaultsName)), file);
         } catch (IOException e) {
             e.printStackTrace();
             instance.getLogger().warning("Failed to create " + file.getName() + ".");
